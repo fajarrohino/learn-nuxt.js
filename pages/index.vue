43206f9b31aebfc0217f9868ae96b23b5ed37e4d
eannoy <fajarrohino14@gmial.com>
2023-11-10 06:55:50 +0700

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <p>apa iya</p>
+  <div class="flex flex-col justify-center items-center m-3">
+    <h6 class="font-bold">Introductions</h6>
+    <div class="my-3">
+      <p>Nama : Fajar Rohino</p>
+      <p>Profesi : Full-stack Developer</p>
+    </div>
+    <p>Click ships untuk melihat data kapal</p>
+    <p>Click add ships untuk menambah data kapal</p>
+    <p>Click action untuk update data kapal</p>
+    <p>Click delete untuk update data kapal</p>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
